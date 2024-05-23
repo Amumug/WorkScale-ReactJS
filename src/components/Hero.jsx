71824e3893e2c8240c732237ea9f8206d6eb66e0
Amumug <fabriali4@gmail.com>
2024-05-23 13:23:01 +0100

    WorkScale-ReactJS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,24 +15,22 @@ const Hero = () => {
       <div className="flex items-end space-x-2 mt-10">
         <img src={teamlight} className="mb-0.5" />
         <h4 className="text-gray-800 text-base font-medium font-['Inter', sans-serif]">Team & Careers</h4>
-
-
-
       </div>
       <h1 className="text-center text-gray-900 text-[32px] font-medium font-['Inter', sans-serif] mt-5">The team from around the globe</h1>
       <h4 className="text-center text-gray-700 text-sm font-medium font-['Inter', sans-serif] mt-4">Creators, designer, engineers, entrepreneurs and and story tellers.</h4>
       <div>
-        <div>
-          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px] mt-8 mb-3 bg-gray-900 rounded-lg justify-center items-center gap-2.5 inline-flex">
+        <div className="flex flex-col md:flex-row md:justify-center items-center space-y-4 md:space-y-0 md:space-x-4 mt-8">
+          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px] cursor-pointer bg-gray-900 rounded-lg justify-center items-center gap-2.5 inline-flex">
             <div className="text-neutral-200 text-lg font-medium font-['Inter', sans-serit]">Open positions</div>
           </div>
 
-          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px]  justify-center items-center gap-2.5 inline-flex">
+          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px] cursor-pointer justify-center items-center gap-2.5 inline-flex">
             <div className="text-gray-901 text-lg font-medium font-['Inter', sans-serif]">Learn more</div>
           </div>
         </div>
-
         <img src={worldmaplight} className="w-full hidden md:block" />
+
+
         <div className="flex-col justify-start items-start inline-flex mt-10">
           <div className="flex-col justify-start items-start gap-3.5 inline-flex ">
             <img src={book} />
