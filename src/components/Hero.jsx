85a64bd5dd6f7ffb1de3da031b80a24004e48538
Amumug <fabriali4@gmail.com>
2024-05-23 13:11:32 +0100

    WorkScale-ReactJS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ import worldmaplight from "../assets/World Map-light.png"
 
 const Hero = () => {
   return (
-    <section className="flex md:flex-row flex-col ${styles.paddingY} justify-center items-center">
+    <section className="flex flex-row flex-col ${styles.paddingY} justify-center items-center">
       <div className="flex items-end space-x-2 mt-10">
         <img src={teamlight} className="mb-0.5" />
         <h4 className="text-gray-800 text-base font-medium font-['Inter', sans-serif]">Team & Careers</h4>
@@ -22,13 +22,16 @@ const Hero = () => {
       <h1 className="text-center text-gray-900 text-[32px] font-medium font-['Inter', sans-serif] mt-5">The team from around the globe</h1>
       <h4 className="text-center text-gray-700 text-sm font-medium font-['Inter', sans-serif] mt-4">Creators, designer, engineers, entrepreneurs and and story tellers.</h4>
       <div>
-        <div className="w-full  md:w-[339px] h-[46px] px-7 py-[5px] mt-8 mb-3 bg-gray-900 rounded-lg justify-center items-center gap-2.5 inline-flex">
-          <div className="text-neutral-200 text-lg font-medium font-['Inter', sans-serit]">Open positions</div>
+        <div>
+          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px] mt-8 mb-3 bg-gray-900 rounded-lg justify-center items-center gap-2.5 inline-flex">
+            <div className="text-neutral-200 text-lg font-medium font-['Inter', sans-serit]">Open positions</div>
+          </div>
+
+          <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px]  justify-center items-center gap-2.5 inline-flex">
+            <div className="text-gray-901 text-lg font-medium font-['Inter', sans-serif]">Learn more</div>
+          </div>
         </div>
 
-        <div className="w-full md:w-[339px] h-[46px] px-7 py-[5px]  justify-center items-center gap-2.5 inline-flex">
-          <div className="text-gray-901 text-lg font-medium font-['Inter', sans-serif]">Learn more</div>
-        </div>
         <img src={worldmaplight} className="w-full hidden md:block" />
         <div className="flex-col justify-start items-start inline-flex mt-10">
           <div className="flex-col justify-start items-start gap-3.5 inline-flex ">
